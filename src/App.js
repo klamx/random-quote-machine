40dbbfffa3a99280b57d98ca7ec59e2867a6f224
Alejandro Largo <largoalejo@gmail.com>
2021-02-10 18:47:29 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: [],
+      num: 0
+    };
+    this.randomNum = this.randomNum.bind(this);
+  }
+
+  render() {
+    const listText = this.state.quotes.map(t => {
+      return (
+        <p key={this.state.quotes.indexOf(t)}>{t.text}</p>
+      );
+    });
+
+    const listAuth = this.state.quotes.map(t => {
+      return (
+        <i key={this.state.quotes.indexOf(t)}>- {t.author}</i>
+      );
+    });
+
+    return (
+      <div className="container-fluid vh-100">
+        <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+          <div className="col-md-5">
+            <div id="quote-box" className="card mt-4">
+              <div className="card-body">
+                <div id="text">
+                  {listText[this.state.num]}
+                </div>
+                <div id="author" className="text-end">
+                  {listAuth[this.state.num]}
+                </div>
+                <div className="btn-group">
+                  <button id="new-quote" onClick={this.randomNum} className="btn btn-primary">New Quote</button>
+                  <button className="btn btn-primary">
+                    <a href="twitter.com/intent/tweet" id="tweet-quote"><i class="fab fa-twitter"></i> Tweet</a>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  componentDidMount() {
+    axios.get(`https://type.fit/api/quotes`)
+      .then(res => {
+        const quotes = res.data;
+        console.log(quotes.length)
+        this.setState({ quotes });
+      });
+  };
+
+  randomNum() {
+    const n = Math.trunc(Math.random() * 1000);
+    this.setState({
+      num: n
+    });
+  }
 }
 
 export default App;
